10c4541f7b66ef458b329d110efe7060ebece0f2
Paul <paul.osbaldo@gmail.com>
2017-11-13 16:28:30 -0600

    se eliminan archivos

--- src/main/java/com/redhat/developers/msa/ola/CustomerApplication.java
+++ src/main/java/com/redhat/developers/msa/ola/CustomerApplication.java
@@ -23,10 +23,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class OlaApplication {
+public class CustomerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OlaApplication.class, args);
+        SpringApplication.run(CustomerApplication.class, args);
     }
 
 }
